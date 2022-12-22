9a5175b7892cb7e19b2e2926d82a3fa34a15ad60
erratinsilentio <kacper.kondracki@wp.pl>
2022-12-22 21:00:25 +0100

    modified project structure

---
+++ 1. Komponenty/zadania/src/components/cards/Cards.tsx
@@ -0,0 +1,32 @@
+import { Data } from "./data";
+import style from "./cards.module.css";
+
+export const Card: React.FC<Data> = ({ data }) => {
+  return (
+    <div className={style.card}>
+      <div className={style.left}>
+        <img className={style.avatar} src={data.imgSrc} alt="avatar" />
+      </div>
+      <div className={style.right}>
+        <p>
+          {data.name} {data.surname}
+        </p>
+        <p>{data.postCode}</p>
+        <p>{data.street}</p>
+        <p>{data.town}</p>
+        <p>{data.subRegion}</p>
+        <p>{data.phoneNumber}</p>
+      </div>
+    </div>
+  );
+};
+
+export const Wrapper: React.FC<Data[]> = ({ cards }) => {
+  return (
+    <ul className={style.cardcontainer}>
+      {cards.map((card) => (
+        <Card data={card} key={card.id} />
+      ))}
+    </ul>
+  );
+};
---
+++ 1. Komponenty/zadania/src/components/cards/data.tsx
@@ -0,0 +1,62 @@
+export interface Data {
+  id?: number;
+  data?: any;
+  imgSrc: string;
+  name: string;
+  surname: string;
+  street: string;
+  postCode: string;
+  town: string;
+  subRegion: string;
+  phoneNumber: string | number;
+}
+const singleData: Data = {
+  imgSrc:
+    "https://images.unsplash.com/photo-1661869535393-872dea2d9f8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+  name: "Adam",
+  surname: "Jochemczyk",
+  street: "TestStreet",
+  postCode: "00-123",
+  town: "Town",
+  subRegion: "Region",
+  phoneNumber: "+48 123 456 789",
+};
+
+export const cards: Data[] = [
+  {
+    id: 1,
+    imgSrc:
+      "https://images.unsplash.com/photo-1661869535393-872dea2d9f8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+    name: "Zigniew",
+    surname: "Herbert",
+    street: "TestStreet",
+    postCode: "00-123",
+    town: "Town",
+    subRegion: "Region",
+    phoneNumber: "+48 123 456 789",
+  },
+  {
+    id: 2,
+    imgSrc:
+      "https://images.unsplash.com/photo-1661869535393-872dea2d9f8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+    name: "Henryk",
+    surname: "Sienkiewicz",
+    street: "TestStreet",
+    postCode: "00-123",
+    town: "Town",
+    subRegion: "Region",
+    phoneNumber: "+48 123 456 789",
+  },
+  {
+    id: 3,
+    imgSrc:
+      "https://images.unsplash.com/photo-1661869535393-872dea2d9f8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+    name: "Wisława",
+    surname: "Szymborska",
+    street: "TestStreet",
+    postCode: "00-123",
+    town: "Town",
+    subRegion: "Region",
+    phoneNumber: "+48 123 456 789",
+  },
+];
