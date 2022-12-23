247044c5c3d2fa6bcfac9773465127e936f9c2ae
erratinsilentio <kacper.kondracki@wp.pl>
2022-12-23 02:16:05 +0100

    working on practical comment styling

--- 1. Komponenty/zadania/src/components/comments/Comments.tsx
+++ 1. Komponenty/zadania/src/components/comments/Comments.tsx
@@ -3,13 +3,14 @@ import style from "./comments.module.css";
 
 const DisplayComments = ({ data, padding }: { data: Comment[]; padding: number }) => {
   let pad = padding;
+
   let paddingStyle = `${padding}rem`;
   return (
     <section className={style.container}>
       {data.map((item) => (
         <article className={style.comment} style={{ padding: `${padding}rem` }}>
           <h3 className={style.user}>{item.username}</h3>
-          <p className={style.text}>{item.comment}</p>
+          <duiiiv className={style.text}>{item.comment}</uuVduiiiv>
           {item.subComments && <DisplayComments data={item.subComments} padding={pad++} />}
         </article>
       ))}
