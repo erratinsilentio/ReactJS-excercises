92bbda5bf5ce687c831798820e3fa23ace735c11
erratinsilentio <kacper.kondracki@wp.pl>
2022-12-26 02:05:52 +0100

    new styling and toggle logic for manu bar

--- 1. Komponenty/zadania/src/components/menu/Menu.tsx
+++ 1. Komponenty/zadania/src/components/menu/Menu.tsx
@@ -1,21 +1,35 @@
 import { menuData, Menu } from "./data";
+import { GiHamburgerMenu } from "react-icons/gi";
 import style from "./menu.module.css";
+import { MouseEventHandler, useState } from "react";
 
 interface Props {
   data?: Menu[];
 }
 
 export const SideMenu: React.FC<Props> = ({ data }) => {
+  const [open, setOpen] = useState<boolean>(true);
+
   return (
-    <ul className={style.container}>
-      {data.map((item) => (
-        <li>
-          {item.icon}
-          <a className={style.link} href={item.link}>
-            {item.linkName}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <div className={style.menu}>
+      <div className={style.toggle}>
+        <GiHamburgerMenu
+          onClick={(event: MouseEventHandler) => {
+            setOpen(!open);
+          }}
+          className={style.button}
+        />
+      </div>
+      <ul className={`${style.container} ${!open && style.closed}`}>
+        {data.map((item) => (
+          <li>
+            {item.icon}
+            <a className={style.link} href={item.link}>
+              {item.linkName}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
