3abb0a867e403957388345248c96f521e6af032c
erratinsilentio <kacper.kondracki@wp.pl>
2022-12-26 02:37:08 +0100

    added hiding comments button and logic

--- 1. Komponenty/zadania/src/components/comments/Comments.tsx
+++ 1. Komponenty/zadania/src/components/comments/Comments.tsx
@@ -1,14 +1,33 @@
 import { Comment } from "./data";
 import style from "./comments.module.css";
+import { SyntheticEvent, useState } from "react";
 
 interface Props {
   data: Comment[];
 }
 export const CommentSection: React.FC<Props> = ({ data }) => {
+  const [open, setOpen] = useState<boolean>(true);
+
+  type Style = {
+    [key: string]: React.CSSProperties;
+  };
+
+  const hidden: React.CSSProperties = () => {
+    // height: !open && '10px';
+    return { display: !open && "none" };
+  };
+
   return (
     <section className={style.container}>
       {data.map((item) => (
-        <article className={style.comment}>
+        <article style={{ height: !open && "10px" }} className={style.comment}>
+          <button
+            onClick={(event: SyntheticEvent) => {
+              setOpen(!open);
+            }}
+          >
+            {open ? "hide" : "show"}
+          </button>
           <h3 className={style.user}>{item.username}</h3>
           <p className={style.text}>{item.comment}</p>
           {item.subComments && <CommentSection data={item.subComments} />}
